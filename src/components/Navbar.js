6c28a7021db651583e3f1e2687233c30ed157e48
web-dev <nathankim137311@gmail.com>
2022-01-16 22:24:14 -0800

    completed basic styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,43 @@
 import React from 'react'
-import { ShoppingCartIcon } from '@heroicons/react/outline';
+import { ShoppingCartIcon, SearchIcon } from '@heroicons/react/outline'
+import legoLogo from '../png/Lego-logo.png'
 
 export default function Navbar() {
     return (
-        <header className='flex flex-row w-full h-16 justify-between items-center px-20 bg-yellow-400 text-black text-md'>
-            <h1 className='font-bold text-lg'>Lego Store</h1>
+        <header className='fixed top-0 flex flex-row w-full h-20 justify-between items-center px-28 bg-yellow-400 text-black text-md'>
+            <div className='flex flex-row items-center'>
+                <a href="/">
+                    <img src={ legoLogo } className='h-14 w-22 mx-4' alt="Lego logo" />
+                </a>
+                <button className='mx-1'>
+                    <SearchIcon className='h-8 w-8 px-1 py-1 bg-white text-black rounded-full' />
+                </button>
+            </div>
             <nav>
                 <ul className="flex flex-row justify-evenly items-center">
                     <li>
-                        <a className='font-bold mx-7' href="/shop">Shop</a>
+                        <a className='font-medium mx-4 hover:underline underline-offset-8 decoration-2' href="/shop">SHOP</a>
                     </li>
                     <li>
-                        <a className='font-bold mx-7' href="/watchlist">Watchlist</a>
-                    </li>
-                    <li className='flex flex-row items-center bg-blue-500 rounded-2xl mx-4 py-1 text-white'>
-                        <a className='font-bold px-4' href="/contact">Contact</a>
+                        <a className='font-medium mx-7 hover:underline underline-offset-8 decoration-2' href="/watchlist">WATCHLIST</a>
                     </li>
                     <li>
-                        <a className='' href="/cart">
-                            <ShoppingCartIcon className='h-7 w-7 mx-6'/> 
+                        <BlueBtn text={ 'contact' } />
+                    </li>
+                    <li className='relative'>
+                        <a href="/cart">
+                            <ShoppingCartIcon className='h-7 w-7 mx-3 my-1'/> 
                         </a>
+                        <div className='flex flex-row justify-center items-center absolute top-0 right-1 h-4 w-4 rounded-full bg-red-600 text-white'>
+                            <small>0</small>
+                        </div>
                     </li>
                 </ul>
             </nav>
         </header>
     )
 }
+
+const BlueBtn = ({ text }) => {
+    return <a className='blue-btn' href="/contact">{ text }</a>
+}; 
