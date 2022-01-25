2994a695f76d4e092e80c7ab51689a55168f1fe4
web-dev <nathankim137311@gmail.com>
2022-01-24 18:58:12 -0800

    refactored productPage media queries

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { BagContext } from './BagContext';
 
@@ -6,6 +6,7 @@ export default function ProductCard({ product }) {
     const {bagArr, totalItemsValue} = useContext(BagContext); 
     const [bag, setBag] = bagArr;
     const [totalItems, setTotalItems] = totalItemsValue;
+    const [limit, setLimit] = useState(false); 
 
     useEffect(() => {
         const sumQauntity = () => {
@@ -19,7 +20,6 @@ export default function ProductCard({ product }) {
     }, [totalItems, bag, setTotalItems])
 
     const addToBag = () => {
-        // push product to array if specific product doesn't exist else increment product quantity by one
         const bagItem = [...bag].find(item => item.item_id === product.item_id);
 
         if (bagItem === undefined) {
@@ -29,7 +29,10 @@ export default function ProductCard({ product }) {
         } else {
             const newBag = [...bag].map(item => {
                 if (item.item_id === product.item_id) {
-                    item.quantity += 1; 
+                    // if item quantity is not 3 then add 1
+                    if (item.quantity !== 3) item.quantity += 1; 
+                    // if quantity is 3 then disable button 
+                    else setLimit(true);
                 }
     
                 return item; 
@@ -39,21 +42,21 @@ export default function ProductCard({ product }) {
     }
 
     return (
-        <div className='flex flex-col items-center xl:my-2 2xl:my-12 xl:mx-2' key={product.item_id}>
-            <Link className='flex flex-row items-center justify-center h-36 w-full pt-2' to={`/shop/${product.item_id}`}>
-                <img className='w-full h-full object-contain' src={product.images[0].split('?')[0]} alt={product.set} />
+        <div className='xxs:flex xxs:flex-col xxs:items-center' key={product.item_id}>
+            <Link className='xxs:flex xxs:flex-row xxs:items-center xxs:justify-center xxs:h-36 xxs:w-full xxs:pt-2' to={`/shop/${product.item_id}`}>
+                <img className='xxs:w-full xxs:h-full xxs:object-contain' src={product.images[0].split('?')[0]} alt={product.set} />
             </Link>
-            <div className='flex flex-col items-center w-full p-2'>
-                <Link className='text-left h-20 w-full hover:underline' to={`/shop/${product.item_id}`}>
+            <div className='xxs:flex xxs:flex-col xxs:items-center xxs:w-full xxs:p-2'>
+                <Link className='xxs:text-left xxs:h-20 xxs:w-full hover:underline' to={`/shop/${product.item_id}`}>
                     {product.set}
                 </Link>
                 {/* ratings determine text color over 3.5 green under 3.5 is orange under 2 is red */}
-                <span className='text-left w-full'>{Math.round(product.rating * 10) / 10} / 5</span>
-                <span className='text-left w-full font-semibold mt-2 mb-2'>${product.price}.99</span>
+                <span className='xxs:text-left xxs:w-full'>{Math.round(product.rating * 10) / 10} / 5</span>
+                <span className='xxs:text-left xxs:w-full xxs:font-semibold xxs:mt-2 xxs:mb-2'>${product.price}.99</span>
                 <button 
-                className='orange-btn mb-2'
+                className={limit ? 'limit-btn': 'orange-btn mb-2'}
                 onClick={addToBag}
-                >Add to Bag</button>
+                >{limit ? 'Limit Reached' : 'Add to Bag'}</button>
             </div>
         </div>
     )
