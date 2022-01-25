8bfe562a870d5881974ebcd1005d57b0bb02adc0
web-dev <nathankim137311@gmail.com>
2022-01-24 18:16:05 -0800

    refactored productDetails component

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -23,18 +23,18 @@ export default function ProductDetails({ product }) {
     }, [bag, totalItems, setTotalItems]);
 
     return (
-        <div className='xxs:p-4 xxs:mt-2 lg:mt-0 lg:pt-0 sm:p-6 lg:w-full lg:px-32'>
-            <div className='xxs:flex xxs:flex-col lg:flex lg:flex-row'>
-                <div className='lg:flex lg:flex-row lg:justify-center lg:items-center bg-white lg:h-78 xxs:w-full xxs:max-w-3xl lg:max-w-6xl lg:border-t-1 lg:border-gray-300'>
-                    <img className='w-full h-full xxs:object-contain lg:w-4/6 lg:my-16' src={images[0]} alt="" />
+        <div className='xxs:p-4 xxs:mt-2'>
+            <div className='xxs:flex xxs:flex-col'>
+                <div className='xxs:bg-white xxs:w-full xxs:max-w-3xl'>
+                    <img className='xxs:w-full xxs:h-full xxs:object-contain' src={images[0]} alt="" />
                 </div>
-                <div className='xxs:mt-3 lg:mt-0 lg:border-t-1 lg:border-gray-300 lg:pt-4 lg:w-1/2'>
-                    <div className='flex flex-row justify-between'>
-                        <h1 className='xxs:text-xl sm:text-2xl sm:font-medium'>{product.set}</h1>
+                <div className='xxs:mt-3'>
+                    <div className='xxs:flex xxs:flex-row xxs:justify-between'>
+                        <h1 className='xxs:text-xl'>{product.set}</h1>
                         <img className='xxs:h-16 xxs:w-28' src={starwarsLogo} alt="" />
                     </div>
-                    <h2 className='xxs:text-xl sm:text-2xl lg:mt-12 font-bold'>${product.price}.99</h2>
-                    <small className='text-sm'>{product.availability}</small> 
+                    <h2 className='xxs:text-xl xxs:font-bold'>${product.price}.99</h2>
+                    <small className='xxs:text-sm'>{product.availability}</small> 
                     <h2>Rating: {Math.round(product.rating * 10) / 10} / 5</h2>
                     <QuantitySelector product={product} bag={bag} setBag={setBag} /> 
                 </div>
@@ -76,21 +76,21 @@ const QuantitySelector = ({ product, bag, setBag }) => {
 
     return (
         <>
-            <div className='flex flex-row justify-start w-fit rounded-md border-1 border-gray-300 my-4'>
+            <div className='xxs:flex xxs:flex-row xxs:justify-start xxs:w-fit xxs:rounded-md xxs:border-1 xxs:border-gray-300 xxs:my-4'>
                 <button 
-                className='flex flex-row justify-center items-center h-12 w-12'
+                className='xxs:flex xxs:flex-row xxs:justify-center xxs:items-center xxs:h-12 xxs:w-12'
                 onClick={handleDecrement}
                 >
-                    <MinusIcon className='h-4 w-4' />
+                    <MinusIcon className='xxs:h-4 xxs:w-4' />
                 </button>
-                <div className='flex flex-row justify-center items-center w-24 border-x-1 border-gray-300'>
+                <div className='xxs:flex xxs:flex-row xxs:justify-center xxs:items-center xxs:w-24 xxs:border-x-1 xxs:border-gray-300'>
                     <span>{quantity}</span>
                 </div>
                 <button 
-                className='flex flex-row justify-center items-center h-12 w-12'
+                className='xxs:flex xxs:flex-row xxs:justify-center xxs:items-center xxs:h-12 xxs:w-12'
                 onClick={handleIncrement}
                 >
-                    <PlusIcon className='h-4 w-4' />
+                    <PlusIcon className='xxs:h-4 xxs:w-4' />
                 </button>
             </div>
             <button className='orange-btn' onClick={addToBag}>
@@ -102,19 +102,19 @@ const QuantitySelector = ({ product, bag, setBag }) => {
 
 const ProductInfo = ({ ages, pieces, item }) => {
     return (
-        <div className='grid grid-cols-3 mt-4 py-2 border-y-1 border-gray-300'>
-            <div className='flex flex-col items-center'>
-                <CakeIcon className='h-10 w-10 text-gray-500 my-2'/>
+        <div className='xxs:grid xxs:grid-cols-3 xxs:mt-4 xxs:py-2 xxs:border-y-1 xxs:border-gray-300'>
+            <div className='xxs:flex xxs:flex-col xxs:items-center'>
+                <CakeIcon className='xxs:h-10 xxs:w-10 xxs:text-gray-500 xxs:my-2'/>
                 <span>{ages}</span>
                 <small>Ages</small>
             </div>
-            <div className='flex flex-col items-center'>
-                <OfficeBuildingIcon className='h-10 w-10 text-gray-500 my-2'/>
+            <div className='xxs:flex xxs:flex-col xxs:items-center'>
+                <OfficeBuildingIcon className='xxs:h-10 xxs:w-10 xxs:text-gray-500 xxs:my-2'/>
                 <span>{pieces}</span>
                 <small>Pieces</small>
             </div>
-            <div className='flex flex-col items-center'>
-                <HashtagIcon className='h-10 w-10 text-gray-500 my-2'/>
+            <div className='xxs:flex xxs:flex-col xxs:items-center'>
+                <HashtagIcon className='xxs:h-10 xxs:w-10 xxs:text-gray-500 xxs:my-2'/>
                 <span>{item.toString().slice(0,5)}</span>
                 <small>Item</small>
             </div>
