393ab43906744a10d3f97c8cb0ffdc8335929d50
web-dev <nathankim137311@gmail.com>
2022-01-24 18:27:32 -0800

    refactored productPage component

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -6,35 +6,51 @@ import { Link } from 'react-router-dom';
 
 export default function ProductPage() {
     const [product, setProduct] = useState([]);
+    const [loading, setLoading] = useState(false); 
+    
     let params = useParams();
     const item_id = params.id;
-
+    
     useEffect(() => {
-        fetchProduct(item_id);
-    }, []); // removed dependency array 
-
-    const fetchProduct = async (id) => {
-        let product = await fetch(`https://lego-star-wars-sets.p.rapidapi.com/api/products/${id}`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "lego-star-wars-sets.p.rapidapi.com",
-                "x-rapidapi-key": "76acaf4aaamshdc4d06215addb4ap16e33cjsn782c56471964"
+        const fetchProduct = async (id) => {
+            try {
+                setLoading(true); 
+                let product = await fetch(`https://lego-star-wars-sets.p.rapidapi.com/api/products/${id}`, {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "lego-star-wars-sets.p.rapidapi.com",
+                        "x-rapidapi-key": "76acaf4aaamshdc4d06215addb4ap16e33cjsn782c56471964"
+                    }
+                });
+                product = await product.json();
+                product[0].quantity = 1; 
+                setProduct(product);
+                setLoading(false); 
+            } catch(err) {
+                console.log(err); 
             }
-        });
-        product = await product.json();
-        product[0].quantity = 1; 
-        setProduct(product);
+        }
+
+        fetchProduct(item_id);
+    }, []); 
+
+    if (loading) {
+        return (
+          <div className='xxs:absolute xxs:left-1/2 xxs:top-1/2 xxs:-translate-x-1/2 xxs:-translate-y-1/2'>
+              <h1>Loading...</h1>
+          </div>
+        );
     }
 
     return (
         <>
-            <div className='flex flex-row xxs:mt-16 lg:mt-20 w-full xxs:p-4 sm:px-6 lg:p-8 lg:px-20 xl:px-28 2xl:px-40 text-sm'>
-                <Link className='text-blue-400 mr-2' to='/shop'>
-                    <h3>Shop &gt;</h3>
+            <div className='xxs:flex xxs:flex-row xxs:mt-16 xxs:w-full xxs:p-4 xxs:text-sm'>
+                <Link className='xxs:text-blue-400 xxs:mr-2' to='/shop'>
+                    <h3>Shop {'>'}</h3>
                 </Link>
-                <h3>{product.length === 0 ? console.log('loading...') : product[0].set}</h3>
+                <h3>{product.length === 0 ? <></> : product[0].set}</h3>
             </div>
-            {product.length === 0 ? console.log('loading...') : <ProductDetails product={product[0]} />}
+            {product.length === 0 ? <></> : <ProductDetails product={product[0]} />}
             <Navbar />
         </>
     )
