e6ef959a7559800ab9ab4d3a21189e529447ff45
web-dev <nathankim137311@gmail.com>
2022-01-26 17:52:04 -0800

    finished basic styling of preview items

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,8 +5,40 @@ import Footer from './components/Footer';
 import starwarsLogo from './png/starwars-logo.png'
 import { Link } from 'react-router-dom';
 import darthVader from './jpg/darthvader.jpg';
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [previewItems, setPreviewItems] = useState([]);
+  const [loading, setLoading] = useState(false); 
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true);
+
+      let data = await fetch('https://lego-star-wars-sets.p.rapidapi.com/api/products?page=1&limit=5', {
+          "method": "GET",
+          "headers": {
+              "x-rapidapi-host": "lego-star-wars-sets.p.rapidapi.com",
+              "x-rapidapi-key": "76acaf4aaamshdc4d06215addb4ap16e33cjsn782c56471964"
+          }
+      });
+      data = await data.json();
+
+      setPreviewItems(data.products);
+      setLoading(false);
+    }
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className='xxs:absolute xxs:left-1/2 xxs:top-1/2 xxs:-translate-x-1/2 xxs:-translate-y-1/2'>
+          <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <>
       <div>
@@ -24,9 +56,38 @@ export default function Home() {
         <div className='xxs:mx-2 xxs:text-center xxs:py-4 xxs:px-4 xxs:text-lg'>
           <span>Launch into the LEGO® Star Wars™ universe!</span>
         </div>
+        <div>
+          <ul className='xxs:flex xxs:flex-row xxs:overflow-x-scroll'>
+            {previewItems.map(item => {
+              return (
+                <ItemThumbnail item={item} />
+              )
+            })}
+          </ul>
+        </div>
       </div>
       <Navbar />
       <Footer />
     </>
   );
+}
+
+const ItemThumbnail = ({ item }) => {
+  return (
+    <div className='xxs:flex xxs:flex-col xxs:px-2 xxs:py-4'>
+      <div className='xxs:flex xxs:flex-row xxs:justify-center xxs:items-center xxs:h-56 xxs:w-56 xxs:border-1 xxs:border-gray-300 xxs:py-2'>
+        <img className='xxs:max-w-full xxs:max-h-full' src={item.images[0].split('?')[0]} alt="" />
+      </div>
+      <div className='xxs:flex xxs:flex-col'>
+        <h3 className='xxs:text-left xxs:mt-2 xxs:h-14'>{item.set}</h3>
+        <div className='xxs:flex xxs:flex-row xxs:justify-between'>
+          <div className='xxs:flex xxs:flex-col'>
+            <span>{Math.round(item.rating * 10) / 10} / 5</span>
+            <span className='xxs:font-semibold'>${item.price}.99</span>
+          </div>
+          <button className='xxs:bg-orange-400 xxs:rounded xxs:px-2 xxs:ml-2 xxs:text-sm xxs:font-medium hover:xxs:bg-white hover:border-2 hover:border-orange-400'>Add to Bag</button>
+        </div>
+      </div>
+    </div>   
+  )
 }
